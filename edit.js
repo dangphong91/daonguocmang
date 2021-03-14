75e24b57561e06b4c32a775e910ce6da476cd10d
dphong08 <dphongg199x@gmail.com>
2021-03-14 21:32:03 +0700

    up

---
+++ edit.js
@@ -0,0 +1,11 @@
+let x = [-3, 5, 1, 3, 2, 10];
+let first = 0;
+let last = x.length - 1;
+while (first < last) {
+    let a = x[first];
+    x[first] = x[last];
+    x[last] = a;
+    first++;
+    last--;
+}
+document.write(x);
